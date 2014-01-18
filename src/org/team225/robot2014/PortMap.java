2d2a821eb7401c91b524f5ffd56077275ec26952
KageRa66b <king_of_hearts533@yahoo.com>
2014-01-18 13:45:20 -0500

    Changed port names Added Catapult.java methods

--- src/org/team225/robot2014/PortMap.java
+++ src/org/team225/robot2014/PortMap.java
@@ -23,9 +23,9 @@ public class PortMap {
     
     public static final int GYRO = 1; // Analog
     
-    public static final int LEFT_LAUNCHER = 1;
-    public static final int RIGHT_LAUNCHER = 2;
-    public static final int SHIFT_ANGLE = 3;
-    public static final int RELEASE = 4;
+    public static final int LEFT_CATAPULT_PISTON = 1;
+    public static final int RIGHT_CATAPULT_PISTON = 2;
+    public static final int SHIFT_CATAPULT_ANGLE = 3;
+    public static final int ARM_RELEASE = 4;
     
 }
--- src/org/team225/robot2014/subsystems/Catapult.java
+++ src/org/team225/robot2014/subsystems/Catapult.java
@@ -6,6 +6,7 @@ package org.team225.robot2014.subsystems;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.Solenoid;
+import org.team225.robot2014.PortMap;
 
 /**
  *
@@ -13,16 +14,29 @@ import edu.wpi.first.wpilibj.Solenoid;
  */
 public class Catapult extends Subsystem {
     
-    Solenoid left = new Solenoid(1);
-    Solenoid right = new Solenoid(2);
-    Solenoid shift = new Solenoid(3);
-    Solenoid release = new Solenoid(4);
+    Solenoid left = new Solenoid(PortMap.LEFT_CATAPULT_PISTON);
+    Solenoid right = new Solenoid(PortMap.RIGHT_CATAPULT_PISTON);
+    Solenoid shift = new Solenoid(PortMap.SHIFT_CATAPULT_ANGLE);
+    Solenoid release = new Solenoid(PortMap.ARM_RELEASE);
     
     public boolean pressurized = false;
     public boolean lockedIn = false;
     public int position = 1;
 
     protected void initDefaultCommand() {
+    }
+    
+    public void setPressurized(boolean on){
+        left.set(on);
+        right.set(on);
+    }
+    
+    public void setAngle(boolean high){
+        shift.set(high);
+    }
+    
+    public void setLock(boolean on){
+        release.set(on);
     }
     
     
